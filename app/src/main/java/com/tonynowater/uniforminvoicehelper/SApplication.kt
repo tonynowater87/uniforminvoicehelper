3aaa5e40383650f1b8b5dff1238fa0c2a08feb78
tonynowater <tonynowater87@gmail.com>
2018-03-23 11:51:38 +0800

    extends DaggerApplication

--- app/src/main/java/com/tonynowater/uniforminvoicehelper/SApplication.kt
+++ app/src/main/java/com/tonynowater/uniforminvoicehelper/SApplication.kt
@@ -1,39 +1,29 @@
 package com.tonynowater.uniforminvoicehelper
 
-import android.app.Activity
-import android.app.Application
 import com.tonynowater.uniforminvoicehelper.dagger.DaggerSAppComponent
-import com.tonynowater.uniforminvoicehelper.dagger.SAppComponent
 import com.tonynowater.uniforminvoicehelper.dagger.SAppModule
 import com.tonynowater.uniforminvoicehelper.dagger.SNetModule
 import com.tonynowater.uniforminvoicehelper.util.uuid.OpenUDID_manager
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import javax.inject.Inject
+import dagger.android.DaggerApplication
 import kotlin.properties.Delegates
 
 /**
  * Created by tonyliao on 2018/3/6.
  */
-class SApplication : Application(), HasActivityInjector {
-
-    @Inject
-    lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
-
-    private lateinit var mAppComponent: SAppComponent
-
-    override fun activityInjector(): AndroidInjector<Activity> = dispatchingActivityInjector
+class SApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
         mInstance = this
         OpenUDID_manager.sync(this)
-        mAppComponent = DaggerSAppComponent.builder()
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerSAppComponent.builder()
                 .sAppModule(SAppModule(this))
                 .sNetModule(SNetModule())
                 .build()
-        mAppComponent.inject(this)
     }
 
     companion object {

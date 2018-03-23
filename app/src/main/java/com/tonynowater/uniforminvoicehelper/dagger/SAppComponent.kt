7f32db6edd4ed2be729f2170114f749f76167156
tonynowater <tonynowater87@gmail.com>
2018-03-23 14:53:11 +0800

    use includes modules

--- app/src/main/java/com/tonynowater/uniforminvoicehelper/dagger/SAppComponent.kt
+++ app/src/main/java/com/tonynowater/uniforminvoicehelper/dagger/SAppComponent.kt
@@ -4,19 +4,13 @@ import android.app.Application
 import com.tonynowater.uniforminvoicehelper.SApplication
 import dagger.Component
 import dagger.android.AndroidInjector
-import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 /**
  * Created by tonyliao on 2018/3/6.
  */
 @Singleton
-@Component(modules = [SActivityModule::class
-                    , SFragmentModule::class
-                    , SNetModule::class
-                    , SAppModule::class
-                    , SRoomModule::class
-                    , AndroidSupportInjectionModule::class])
+@Component(modules = [SAppModule::class])
 interface SAppComponent : AndroidInjector<SApplication> {
     fun application():Application
 }
--- app/src/main/java/com/tonynowater/uniforminvoicehelper/dagger/SAppModule.kt
+++ app/src/main/java/com/tonynowater/uniforminvoicehelper/dagger/SAppModule.kt
@@ -3,15 +3,20 @@ package com.tonynowater.uniforminvoicehelper.dagger
 import android.app.Application
 import dagger.Module
 import dagger.Provides
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 /**
  * Created by tonyliao on 2018/3/22.
  */
-@Module
+@Module(includes = [SActivityModule::class
+                    , SFragmentModule::class
+                    , SNetModule::class
+                    , SRoomModule::class
+                    , AndroidSupportInjectionModule::class])
 class SAppModule(var mApplication: Application) {
 
     @Provides
     @Singleton
-    fun provideApplication():Application = mApplication
+    fun provideApplication(): Application = mApplication
 }
